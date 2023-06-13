e8d6a335f72007a8a3829661b9cf79b3de577554
Alexandr <alexandrdryzd@yandex.ru>
2023-06-13 19:47:36 +0300

    11.1 11.3

--- lection 11/task_1.py
+++ lection 11/task_1.py
@@ -9,3 +9,31 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+sbis_site = 'https://sbis.ru/'
+driver = webdriver.Chrome()
+try:
+    print('Переходим на сайт')
+    driver.get(sbis_site)
+    print('Перейти в раздел Контакты')
+    contacts = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header__menu-item-1')
+    contacts.click()
+    print('Клик по банеру Тензор')
+    baner = driver.find_element(By.CSS_SELECTOR, '.sbisru-Contacts__logo-tensor')
+    baner.click()
+    print('переход на tensor.ru')
+    driver.switch_to.window(driver.window_handles[1])
+    print('Проверка наличия блока новости "Сила в людях"')
+    news = driver.find_element(By.CSS_SELECTOR,
+                               ".tensor_ru-Index__block4-content "
+                               "[class='tensor_ru-Index__card-title tensor_ru-pb-16']")
+    assert news.text == 'Сила в людях'
+    print('Переход в блок "Подробнее", должен открыться https://tensor.ru/about')
+    about = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__card-text [href="/about"]')
+    about.location_once_scrolled_into_view
+    about.click()
+    assert "https://tensor.ru/about" in driver.current_url, "Неверный сайт"
+    print('SUCCESS')
+
+finally:
+    driver.quit()
--- lection 11/task_with_star.py
+++ lection 11/task_with_star.py
@@ -9,3 +9,39 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pathlib import Path
+from time import sleep
+
+
+path = Path.cwd()
+
+sbis_link = 'https://sbis.ru/'
+driver = webdriver.Chrome()
+
+try:
+    print('Переходим на сайт')
+    driver.get(sbis_link)
+    sleep(3)
+    footer = driver.find_element(By.CSS_SELECTOR, ".sbisru-Footer__container")
+    driver.execute_script("return arguments[0].scrollIntoView(true);", footer)
+    sleep(3)
+    print('в Footer нашли "Скачать СБИС"')
+    plugin_link = driver.find_element(By.XPATH, "//a[contains(@class, 'sbisru-Footer__link') and "
+                                                 "contains(text(), 'Скачать СБИС')]")
+    plugin_link.click()
+    sleep(3)
+    print('Перешли на вкладку "СБИС Плагин"')
+    plugin = driver.find_element(By.CSS_SELECTOR, '[data-id="plugin"]')
+    plugin.click()
+    sleep(3)
+    print('Скачивание файла')
+    download_links = driver.find_elements(By.CSS_SELECTOR, '[data-for="plugin"] .sbis_ru-DownloadNew-loadLink')
+    download_links[0].click()
+    sleep(3)
+    plugin_file = Path(path, 'sbisplugin-setup-web.exe')
+    assert plugin_file.exists(), 'Плагин не скачался!'
+    for file in path.glob('sbisplugin-setup-web.exe'):
+        print(f'Размер скачанного файла: {round(int(file.stat().st_size) / 1048576, 2)} мб')
+    print('SUCCESS')
+finally:
+    driver.quit()
