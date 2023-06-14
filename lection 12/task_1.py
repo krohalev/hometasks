49bbe93b73e288e2b54565f3413102719cc1ad9f
Alexandr <alexandrdryzd@yandex.ru>
2023-06-14 17:16:12 +0300

    11

--- lection 12/task_1.py
+++ lection 12/task_1.py
@@ -1,1 +1,7 @@
 from atf.ui import *
+
+
+class TestSendMessage(TestCaseUI):
+
+    def test_01_sending_message(self):
+        """Отправка сообщения самому себе, и последующие удаление этого сообщения"""
